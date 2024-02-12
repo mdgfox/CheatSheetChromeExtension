812e6a9eab41449898f6e46b082bfd8657e07c89
Alex Khmelev <sashaaka4774@gmail.com>
2024-02-12 10:28:50 +0100

    Remove logoSources from build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -25,7 +25,18 @@ module.exports = {
    },
    plugins: [
       new CopyPlugin({
-         patterns: [{from: ".", to: ".", context: "public", globOptions: { ignore: ['**/readme.png', '**/palette_9.png'] }}],
+         patterns: [{
+            from: ".",
+            to: ".",
+            context: "public",
+            globOptions: {
+               ignore: [
+                   '**/readme.png',
+                  '**/palette_9.png',
+                  '**/logoSources',
+               ]
+            }
+         }],
       }),
    ],
 };
